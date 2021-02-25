adf503e1d4c78d6c982e84a15428d6215383cafe
Kamil <kamil.vakhitov95@gmail.com>
2021-02-25 20:56:16 +0500

    add slider

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/left.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/right.png"></button>',
+  });
+
+});
